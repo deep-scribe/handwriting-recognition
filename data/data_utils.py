3b74b3f0fe99f81c00d54e539be843547feebf17
Junshen Kevin Chen <kevinehc@gmail.com>
2019-10-24 16:37:55 -0700

    data utils to load from parent dir of all subject subdir into one single dataframe

---
+++ data/data_utils.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import os
+import string
+
+COLUMNS = [
+    'id', 'td',
+    'yaw', 'pitch', 'roll',
+    'ax', 'ay', 'az',
+    'gx', 'gy', 'gz',
+    'mx', 'my', 'mz',
+]
+
+
+def load_one_char_csv(filename):
+    '''
+    loads a {char}.csv 
+    return a pandas dataframe
+    '''
+    df = pd.read_csv(
+        filename,
+        header=None,
+        names=COLUMNS
+    )
+    return df
+
+
+def append_identifiers(df, label, subject_id):
+    '''
+    Append a columns to the dataframe `df`
+    df: a dataframe to modify
+    label: str label
+    subject_id: str subject identifier
+    '''
+    df['subject_id'] = subject_id
+    df['label'] = label
+
+
+def load_subject(subject_path):
+    '''
+    load all {char}.csv from a directory, append label, 
+    return a merged pd dataframe
+    '''
+
+    dfs = []
+
+    for ch in string.ascii_lowercase:
+        filename = f'{ch}.csv'
+        chardf = load_one_char_csv(
+            os.path.join(subject_path, filename)
+        )
+        append_identifiers(chardf, label=ch, subject_id=subject_path)
+        dfs.append(chardf)
+
+    return pd.concat(dfs, ignore_index=True)
+
+
+def load_all_subjects(parent_path):
+    '''
+    load csvs of all subdirs 
+    return one single pandas dataframe
+    '''
+    dfs = []
+
+    for root, dirs, files in os.walk(parent_path):
+        for subject_path in dirs:
+            dfs.append(load_subject(
+                os.path.join(parent_path, subject_path)
+            ))
+
+    return pd.concat(dfs, ignore_index=True)
+
+
+# def main():
+#     df = load_all_subjects('raw_data')
+#     print(df)
+
+
+# if __name__ == "__main__":
+#     main()
--- data/readme.md
+++ data/readme.md
@@ -5,17 +5,6 @@
 - Directory name must be unique and should be reasonably descriptive
 - Each directory should contain 26 csv files only, with names being `{character}.csv`
 
-## Preprocessing
-
-Under construction.
-
-Given a source directory, a destination directory:
-
-- read all 26 files from the source
-- concate all rows with respective label
-- create `{source_dir_name}.csv` in `dest_dir` if not exist
-- dump labeled data to the above file
-
 ## Visualizer
 
 Under construction.
