6b2939a2b76f5f93c8433c0d465c4b52c2f0965c
Kelly He <ame@DN51slds.SUNet>
2019-12-09 17:05:02 -0800

    collect subject

--- src/rnn.py
+++ src/rnn.py
@@ -12,7 +12,7 @@ from collections import defaultdict
 print(torch.cuda.is_available())
 
 #cell 2
-trainx, devx, testx, trainy, devy, testy = data_loader.load_all_classic_random_split(flatten=False)
+trainx, devx, testx, trainy, devy, testy = data_loader.load_all_subject_split(flatten=False)
 
 #cell 3
 trainx, trainy = data_loader.augment_train_set(trainx, trainy, augment_prop=3, is_flattened=False)
@@ -144,7 +144,7 @@ testacc, testloss
 hist['testacc'] = testacc
 hist['testloss'] = testloss
 
-with open('../output/rnn/rnn_hist_random.json', 'w') as f:
+with open('../output/rnn/rnn_hist_subject.json', 'w') as f:
     json.dump(hist, f)
 
 #cell 9
