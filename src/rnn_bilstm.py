22f92e42b5f6de4ef6be41527dbbdc4c9f683531
Kelly He <ame@DN0a1e8ac9.SUNet>
2020-02-16 23:57:14 -0800

    add non resample option

--- src/rnn_bilstm.py
+++ src/rnn_bilstm.py
@@ -89,13 +89,19 @@ class Net(nn.Module):
 
 
 
-experiment_type, trial = sys.argv
-filename = experiment_type + '_' + trial
+experiment_type, resampled, trial = sys.argv
+filename = experiment_type + '_' + resampled + '_' + trial
 
 if experiment_type == "subject":
-    trainx, devx, testx, trainy, devy, testy = data_loader.load_all_subject_split(resampled = False, flatten=False)
+    if resampled == "resampled":
+        trainx, devx, testx, trainy, devy, testy = data_loader.load_all_subject_split(resampled = False, flatten=False)
+    else:
+        trainx, devx, testx, trainy, devy, testy = data_loader.load_all_subject_split(resampled = True, flatten=False)
 else:
-    trainx, devx, testx, trainy, devy, testy = data_loader.load_all_subject_split(resampled = False, flatten=False)
+    if resampled == "resampled":
+        trainx, devx, testx, trainy, devy, testy = data_loader.load_all_random_split(resampled = False, flatten=False)
+    else:
+        trainx, devx, testx, trainy, devy, testy = data_loader.load_all_random_split(resampled = True, flatten=False)
 
 trainx, trainy = data_loader.augment_train_set(trainx, trainy, augment_prop=3, is_flattened=False)
 print(trainx.shape, devx.shape, testx.shape, trainy.shape, devy.shape, testy.shape)
