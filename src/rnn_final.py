40815f91e0708c35e7bef73364d13c23d71cde9e
Kelly He <ame@DN51t4qa.SUNet>
2020-02-29 19:43:36 -0800

    best acc

--- src/rnn_final.py
+++ src/rnn_final.py
@@ -212,7 +212,7 @@ def main():
 
     hist = defaultdict(list)
     best_acc = 0
-    for epoch in range(1):  # loop over the dataset multiple times
+    for epoch in range(100):  # loop over the dataset multiple times
         running_loss = 0.0
         for i, data in enumerate(trainloader):
             print(f'{i if i%20==0 else ""}.', end='')
