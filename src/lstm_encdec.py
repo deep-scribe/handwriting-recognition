12abe29d0d1d042194ed6e5fb7123687a768a332
Junshen Kevin Chen <kevinehc@gmail.com>
2020-03-02 14:24:38 -0800

    lstm1

--- src/lstm_encdec.py
+++ src/lstm_encdec.py
@@ -1,4 +1,3 @@
-# cell 0
 import torch
 import data_loader_upper
 import torch.nn as nn
@@ -7,14 +6,12 @@ import torch.optim as optim
 import os
 import json
 from collections import defaultdict
-# import autoencoder
 import numpy as np
 import sys
 from torch.nn.utils.rnn import pad_sequence
 import data_augmentation
 import data_flatten
-
-# cell 1
+from torch.nn.utils.rnn import pack_sequence
 
 
 def get_dataloader(x, y, batch_size):
@@ -66,43 +63,9 @@ def acc_loss(net, data_loader, criterion):
             total_loss += criterion(outputs, y.long()).item() * len(x)
     return correct / total, total_loss / total
 
-# cell 7
-
-
-class Net(nn.Module):
-    def __init__(self, input_dim, hidden_dim, n_layers):
-        super(Net, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.n_layers = n_layers
-        self.lstm = nn.LSTM(input_dim, hidden_dim, n_layers,
-                            batch_first=True, bidirectional=True)
-        # self.dropout = nn.Dropout(0.1)
-        self.fc = nn.Linear(hidden_dim*2, 800, bias=True)
-        self.fc2 = nn.Linear(800, 500, bias=True)
-        self.fc3 = nn.Linear(500, 26, bias=True)
-
-    def forward(self, x):
-        init_h = torch.randn(self.n_layers*2, x.shape[0], self.hidden_dim)
-        init_c = torch.randn(self.n_layers*2, x.shape[0], self.hidden_dim)
-        if torch.cuda.is_available():
-            init_h = init_h.cuda()
-            init_c = init_c.cuda()
-        x = x.permute(0, 2, 1)
-        out, _ = self.lstm(x, (init_h, init_c))
-        # out = self.dropout(out)
-        # print("inter: ", out.shape)
-        out = self.fc(out[:, -1, :])
-        out = torch.nn.functional.relu(out)
-        out = self.fc2(out)
-        out = torch.nn.functional.relu(out)
-        out = self.fc3(out)
-        # print("out: ", out.shape)
-        return out
-
 
 def get_net(checkpoint_path):
-    net = Net(3, 100, 5)
+    net = LSTMencdec(n_channels=3)
     if torch.cuda.is_available():
         net.load_state_dict(torch.load(checkpoint_path))
     else:
@@ -123,72 +86,81 @@ def get_prob(net, input):
     return logit
 
 
+class LSTMencdec(nn.Module):
+    def __init__(self, n_channels, hidden_dim=500, n_layers=3, bidirectional=True):
+        super(LSTMencdec, self).__init__()
+        self.n_channels = n_channels
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.num_dir = 2 if bidirectional else 1
+        self.lstm = nn.LSTM(
+            n_channels, hidden_dim, n_layers,
+            batch_first=True,
+            bidirectional=bidirectional)
+        self.fc = nn.Linear(hidden_dim*n_layers*self.num_dir, 200, bias=True)
+        self.fc2 = nn.Linear(200, 26, bias=True)
+
+    def forward(self, x):
+        init_h = torch.randn(
+            self.n_layers * self.num_dir,
+            x.shape[0],
+            self.hidden_dim)
+        init_c = torch.randn(
+            self.n_layers * self.num_dir,
+            x.shape[0],
+            self.hidden_dim)
+        if torch.cuda.is_available():
+            init_h = init_h.cuda()
+            init_c = init_c.cuda()
+
+        x = x.permute(0, 2, 1)
+
+        output, (h_n, c_n) = self.lstm(x, (init_h, init_c))
+        # c_n (num_layers * num_directions, batch, hidden_size)
+        c_n = c_n.permute(1, 0, 2)
+        # c_n (batch, num_layers * num_directions, hidden_size)
+        c_n_flat = c_n.reshape(-1, self.n_layers*2*self.hidden_dim)
+        # c_n (batch, num_layers * num_directions * hidden_size)
+        out = self.fc(c_n_flat)
+        out = torch.nn.functional.relu(out)
+        out = self.fc2(out)
+        return out
+
+
 def main():
     print(torch.cuda.is_available())
     print(sys.argv[1:])
-    _, experiment_type, resampled, trial = sys.argv
-    filename = experiment_type + '_' + resampled + '_' + trial
+    _, trial = sys.argv
+    filename = '_' + trial
 
-    if experiment_type == "subject":
-        trainx, devx, testx, trainy, devy, testy = data_loader_upper.load_all_subject_split(
-            resampled=False, flatten=False, keep_idx_and_td=True)
-    else:
-        trainx, devx, testx, trainy, devy, testy = data_loader_upper.load_all_classic_random_split(
-            resampled=False, flatten=False, keep_idx_and_td=True)
+    trainx, devx, testx, trainy, devy, testy = data_loader_upper.load_all_classic_random_split(
+        resampled=True, flatten=False, keep_idx_and_td=False)
 
-    print(trainx.shape, devx.shape, testx.shape,
-          trainy.shape, devy.shape, testy.shape)
+    # def aug_head_tail(x, y):
+    #     x, y = data_augmentation.augment_head_tail_noise(
+    #         x, y, augment_prop=1)
+    #     x = data_flatten.resample_dataset_list(x)
+    #     x = np.array(x)
+    #     return x, y
 
-    def aug_head_tail(x, y):
-        x, y = data_augmentation.augment_head_tail_noise(
-            x, y, augment_prop=5)
-        x = data_flatten.resample_dataset_list(x)
-        x = np.array(x)
-        return x, y
+    # trainx, trainy = aug_head_tail(trainx, trainy)
+    # devx, devy = aug_head_tail(devx, devy)
+    # testx, testy = aug_head_tail(testx, testy)
 
-    trainx, trainy = aug_head_tail(trainx, trainy)
-    devx, devy = aug_head_tail(devx, devy)
-    testx, testy = aug_head_tail(testx, testy)
+    trainx, trainy = data_loader_upper.augment_train_set(
+        trainx, trainy, augment_prop=20,
+        is_flattened=False, resampled=True)
 
-    if resampled == "resampled":
-        trainx, trainy = data_loader_upper.augment_train_set(
-            trainx, trainy, augment_prop=5,
-            is_flattened=False, resampled=True)
-        trainx, devx, testx = pad_all_x(trainx, devx, testx)
-    else:
-        trainx, trainy = data_loader_upper.augment_train_set(
-            trainx, trainy, augment_prop=1, is_flattened=False, resampled=False)
-        trainx, devx, testx = pad_all_x(trainx, devx, testx)
-    print(trainx.shape, devx.shape, testx.shape,
-          trainy.shape, devy.shape, testy.shape)
-
-    #
-    # trainx = encode(trainx, encoder)
-    # devx = encode(devx, encoder)
-    # testx = encode(testx, encoder)
-    # print(trainx.shape, devx.shape, testx.shape, trainy.shape, devy.shape, testy.shape)
-    # del encoder
-
-    # cell 4
-    BATCH_SIZE = 3000
-
+    BATCH_SIZE = 500
     trainloader = get_dataloader(trainx, trainy, BATCH_SIZE)
     devloader = get_dataloader(devx, devy, BATCH_SIZE)
     testloader = get_dataloader(testx, testy, BATCH_SIZE)
 
-    # cell 5
-    sample_size, num_feature, num_channel = trainx.shape
-    print(sample_size, num_feature, num_channel)
-
-    # cell 6
-
-    net = Net(num_channel, 100, 5)
+    net = LSTMencdec(n_channels=3)
     if torch.cuda.is_available():
         net.cuda()
 
-    # cell 8
     criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.SGD(net.parameters(), lr=0.00001, momentum=0.9)
     optimizer = optim.AdamW(net.parameters(), weight_decay=0.005)
 
     hist = defaultdict(list)
@@ -196,18 +168,16 @@ def main():
     for epoch in range(200):  # loop over the dataset multiple times
         running_loss = 0.0
         for i, data in enumerate(trainloader):
-            print(f'{i if i%20==0 else ""}.', end='')
+            print(f'{i if i%20==0 else ""}.', end='', flush=True)
 
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
+
             if torch.cuda.is_available():
                 inputs = inputs.cuda()
                 labels = labels.cuda()
 
-            # zero the parameter gradients
             optimizer.zero_grad()
-
-            # forward + backward + optimize
 
             outputs = net(inputs.float())
             loss = criterion(outputs, labels.long())
@@ -225,13 +195,13 @@ def main():
         print(f'        trainloss={trainloss} devloss={devloss}')
         if best_loss > devloss:
             best_loss = devloss
-            torch.save(net.state_dict(), "../saved_model/rnn_final/" +
-                       "rnn_final_" + filename + ".pth")
+            torch.save(net.state_dict(), "../saved_model/lstm_encdec/" +
+                       "lstm_encdec_" + filename + ".pth")
 
     print('Finished Training', 'Best Dev Loss', best_loss)
 
-    net.load_state_dict(torch.load("../saved_model/rnn_final/" +
-                                   "rnn_final_" + filename + ".pth"))
+    net.load_state_dict(torch.load("../saved_model/lstm_encdec/" +
+                                   "lstm_encdec_" + filename + ".pth"))
 
     testacc, testloss = acc_loss(net, testloader, nn.CrossEntropyLoss())
     testacc, testloss
@@ -239,7 +209,7 @@ def main():
     hist['testloss'] = testloss
     print(f'test loss={testloss} test acc={testacc}')
 
-    with open('../output/rnn_final/rnn_final_' + filename + '.json', 'w') as f:
+    with open('../output/lstm_encdec/lstm_encdec_' + filename + '.json', 'w') as f:
         json.dump(hist, f)
 
 
--- src/word_search_test.py
+++ src/word_search_test.py
@@ -13,7 +13,7 @@ import cnn
 import random
 
 # MODEL_WEIGHT_PATH = '../saved_model/rnn_bilstm/rnn_bilstm_random_resampled_0.pth'
-MODEL_WEIGHT_PATH = '../saved_model/rnn_final/rnn_final_random_resampled_14.pth'
+MODEL_WEIGHT_PATH = '../saved_model/rnn_final/rnn_final_random_resampled_15.pth'
 
 '''
 Test the feasibility to use trajectory_search to reconstruct word
@@ -58,82 +58,81 @@ if __name__ == "__main__":
     # print(wrong)
     # print(np.sum(wrong))
 
-    # word_df = data_utils.load_subject('../data_words/kevin_tip_2')
-    # # word_df = data_utils.load_subject('../data_words/kevin_t_words')
-    # wordxs, wordys = data_utils.get_calibrated_yprs_samples(
-    #     word_df, resampled=False, flatten=False,
-    #     is_word_samples=True, keep_idx_and_td=True
-    # )
+    word_df = data_utils.load_subject('../data_words/kevin_tip')
+    # word_df = data_utils.load_subject('../data_words/kevin_t_words')
+    wordxs, wordys = data_utils.get_calibrated_yprs_samples(
+        word_df, resampled=False, flatten=False,
+        is_word_samples=True, keep_idx_and_td=True
+    )
 
-    # for idx in range(len(wordys)):
-    #     x = wordxs[idx]
-    #     y = wordys[idx]
-    #     # if 'a' in y or 'A' in y:
-    #     #     continue
-    #     NUM_PART = len(x) // 10 + 5
-    #     print(NUM_PART)
-    #     trajs = word_search.word_search(x, NUM_PART, 10, model)
-    #     print(f'predicting {y}')
+    for idx in range(len(wordys)):
+        x = wordxs[idx]
+        y = wordys[idx]
 
-    #     for i, (likelihood, traj) in enumerate(trajs):
-    #         word = ''
-    #         for seg_begin, seg_end, pred, prob in traj:
-    #             word += chr(pred+97)
-    #         print(f', pred {i}: likelihood {likelihood}', word)
-
-    TARGET_WORDS = [
-        'abe', 'ace', 'am', 'in'
-        # 'qw', 'op', 'yo', 'txr'
-    ]
-
-    char_df = data_utils.load_subject('../data_upper/kevin_tip_first')
-    calibration_yprs = data_utils.get_yprs_calibration_vector(char_df)
-
-    for target_word in TARGET_WORDS:
-        print('-'*80)
-        print(f'checking word {target_word}')
-        print('-'*80)
-        sample_chs = []
-        for i, ch in enumerate(target_word):
-            sample_df = data_utils.get_random_sample_by_label(char_df, ch)
-            sample_yprs = sample_df[data_utils.YPRS_COLUMNS].to_numpy()
-            # calibrate
-            sample_yprs = sample_yprs - calibration_yprs - sample_yprs[0]
-
-            # check each char predicted is correct
-            # resample to test forward
-            sample_id_col = sample_df[data_utils.ID_COLUMN].to_numpy().reshape(
-                (-1, 1))
-            sample_td_col = sample_df[data_utils.TIME_DELTA_COLUMN].to_numpy().reshape(
-                (-1, 1))
-            yprs_with_id_td = np.hstack(
-                (sample_id_col, sample_td_col, sample_yprs))
-            sample_yprs = data_flatten.resample_sequence(
-                yprs_with_id_td,
-                is_flatten_ypr=False,
-                feature_num=100
-            )
-            yhat = rnn_final.get_prob(model, torch.tensor([sample_yprs.T]))
-            pred = chr(97+np.argmax(yhat))
-            print(f'expect [{ch}] predict [{pred}]')
-
-            sample_chs.append(yprs_with_id_td)
-
-            # # insert noise
-            num_noise_frame = random.randint(6, 10)
-            noise_start = random.randint(0, yprs_with_id_td.shape[0])
-            noise = yprs_with_id_td[
-                noise_start:noise_start+num_noise_frame, :]
-            sample_chs.append(noise)
-
-        # concat each char sequence to word sequence
-        x = np.vstack(sample_chs)
-
-        NUM_PART = int(x.shape[0] / 10) + 5
+        NUM_PART = len(x) // 10 + 5
+        print(NUM_PART)
         trajs = word_search.word_search(x, NUM_PART, 10, model)
+        print(f'predicting {y}')
 
         for i, (likelihood, traj) in enumerate(trajs):
             word = ''
             for seg_begin, seg_end, pred, prob in traj:
                 word += chr(pred+97)
-            print(f'pred {i}:', word)
+            print(f', pred {i}: likelihood {likelihood}', word)
+
+    # TARGET_WORDS = [
+    #     'abe', 'ace', 'am', 'in'
+    #     # 'qw', 'op', 'yo', 'txr'
+    # ]
+
+    # char_df = data_utils.load_subject('../data_upper/kevin_tip_first')
+    # calibration_yprs = data_utils.get_yprs_calibration_vector(char_df)
+
+    # for target_word in TARGET_WORDS:
+    #     print('-'*80)
+    #     print(f'checking word {target_word}')
+    #     print('-'*80)
+    #     sample_chs = []
+    #     for i, ch in enumerate(target_word):
+    #         sample_df = data_utils.get_random_sample_by_label(char_df, ch)
+    #         sample_yprs = sample_df[data_utils.YPRS_COLUMNS].to_numpy()
+    #         # calibrate
+    #         sample_yprs = sample_yprs - calibration_yprs - sample_yprs[0]
+
+    #         # check each char predicted is correct
+    #         # resample to test forward
+    #         sample_id_col = sample_df[data_utils.ID_COLUMN].to_numpy().reshape(
+    #             (-1, 1))
+    #         sample_td_col = sample_df[data_utils.TIME_DELTA_COLUMN].to_numpy().reshape(
+    #             (-1, 1))
+    #         yprs_with_id_td = np.hstack(
+    #             (sample_id_col, sample_td_col, sample_yprs))
+    #         sample_yprs = data_flatten.resample_sequence(
+    #             yprs_with_id_td,
+    #             is_flatten_ypr=False,
+    #             feature_num=100
+    #         )
+    #         yhat = rnn_final.get_prob(model, torch.tensor([sample_yprs.T]))
+    #         pred = chr(97+np.argmax(yhat))
+    #         print(f'expect [{ch}] predict [{pred}]')
+
+    #         sample_chs.append(yprs_with_id_td)
+
+    #         # # insert noise
+    #         num_noise_frame = random.randint(6, 10)
+    #         noise_start = random.randint(0, yprs_with_id_td.shape[0])
+    #         noise = yprs_with_id_td[
+    #             noise_start:noise_start+num_noise_frame, :]
+    #         sample_chs.append(noise)
+
+    #     # concat each char sequence to word sequence
+    #     x = np.vstack(sample_chs)
+
+    #     NUM_PART = int(x.shape[0] / 10) + 5
+    #     trajs = word_search.word_search(x, NUM_PART, 10, model)
+
+    #     for i, (likelihood, traj) in enumerate(trajs):
+    #         word = ''
+    #         for seg_begin, seg_end, pred, prob in traj:
+    #             word += chr(pred+97)
+    #         print(f'pred {i}:', word)
