7c1ea1ed9a2994f162cce4bdc7fcd3f0d77e38bf
Kelly He <ame@DN0a1e8ac9.SUNet>
2020-03-12 15:59:22 -0700

    siamese train

--- src/train_siamese.py
+++ src/train_siamese.py
@@ -51,7 +51,7 @@ class ContrastiveLoss(torch.nn.Module):
 
     def forward(self, output1, output2, label1, label2):
         euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
-        print(euclidean_distance)
+        # print(euclidean_distance)
         # loss_contrastive = torch.mean((label1==label2).float() * torch.pow(euclidean_distance, 2) +
         #                               (1-(label1==label2).float()) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
 
