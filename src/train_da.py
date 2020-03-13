f49d5cbb49328ef2232f96761fa8c00a5404ee58
Kelly He <ame@DN0a1e8ac9.SUNet>
2020-03-12 17:27:12 -0700

    da

--- src/train_da.py
+++ src/train_da.py
@@ -30,7 +30,7 @@ WEIGHT_DIR = '../saved_model/'
 
 class DANet(torch.nn.Module):
     def __init__(self, input_dim):
-        super(SiameseNet, self).__init__()
+        super(DANet, self).__init__()
         self.fc1 = nn.Linear(input_dim, input_dim/2, bias = True)
         self.fc2 = nn.Linear(input_dim/2, 27, bias = True)
 
